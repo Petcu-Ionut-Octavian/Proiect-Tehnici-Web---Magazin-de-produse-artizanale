27b381e63ac15df315948e2b9163cb2a3ea858c4
Petcu-Ionut-Octavian <ionutoctavianpetcu@gmail.com>
2026-03-24 01:38:20 +0200

    Actualizat meniul + CSS responsive

--- index.js
+++ index.js
@@ -1,41 +1,108 @@
 const express= require("express");
 const path= require("path");
+const fs=require("fs");
+const sass=require("sass");
 
 app= express();
 app.set("view engine", "ejs")
+
+
+
+obGlobal={
+    obErori:null,
+    obImagini:null,
+    folderScss: path.join(__dirname,"resurse/scss"),
+    folderCss: path.join(__dirname,"resurse/css"),
+    folderBackup: path.join(__dirname,"backup"),
+}
 
 console.log("Folder index.js", __dirname);
 console.log("Folder curent (de lucru)", process.cwd());
 console.log("Cale fisier", __filename);
 
+app.use("/resurse",express.static(path.join(__dirname, "resurse")));
 
-app.get("/", function(req, res) {
+
+app.get("/", function(req, res){
     res.render("pagini/index");
 });
 
-app.get("/despre", function(req, res) {
+app.get("/despre", function(req, res){
     res.render("pagini/despre");
 });
 
-app.use("/resurse", express.static(path.join(__dirname, "resurse")));
 
 
-app.get("/cale", function(req, res) {
-    console.log("Am primit o cerere GET pe /cale");
-    res.send("Raspuns la <b style='color: red;'>cererea GET pe /cale</b>");
-});
 
-app.get("/cale2", function(req, res) {
-    res.write("Raspuns la <b style='color: red;'>cererea GET pe /cale2</b>");
-    res.write("<br>Acesta este al doilea rand din raspuns");
-    res.end();
-});
+function initErori(){
+    let continut = fs.readFileSync(path.join(__dirname,"resurse/json/erori.json")).toString("utf-8");
+    let erori=obGlobal.obErori=JSON.parse(continut)
+    let err_default=erori.eroare_default
+    err_default.imagine=path.join(erori.cale_baza, err_default.imagine)
+    for (let eroare of erori.info_erori){
+        eroare.imagine=path.join(erori.cale_baza, eroare.imagine)
+    }
 
-app.get("/cale2/:a/:b", function(req, res) {
-    console.log("Suma: ", parseInt(req.params.a) + parseInt(req.params.b));
-    res.send("Suma: " + (parseInt(req.params.a) + parseInt(req.params.b)));
-});
+}
+initErori()
 
+
+
+function compileazaScss(caleScss, caleCss){
+    if(!caleCss){
+
+        let numeFisExt=path.basename(caleScss); // "folder1/folder2/a.scss" -> "a.scss"
+        let numeFis=numeFisExt.split(".")[0]   /// "a.scss"  -> ["a","scss"]
+        caleCss=numeFis+".css"; // output: a.css
+    }
+    
+    if (!path.isAbsolute(caleScss))
+        caleScss=path.join(obGlobal.folderScss,caleScss )
+    if (!path.isAbsolute(caleCss))
+        caleCss=path.join(obGlobal.folderCss,caleCss )
+    
+    let caleBackup=path.join(obGlobal.folderBackup, "resurse/css");
+    if (!fs.existsSync(caleBackup)) {
+        fs.mkdirSync(caleBackup,{recursive:true})
+    }
+    
+    // la acest punct avem cai absolute in caleScss si  caleCss
+
+    let numeFisCss=path.basename(caleCss);
+    if (fs.existsSync(caleCss)){
+        fs.copyFileSync(caleCss, path.join(obGlobal.folderBackup, "resurse/css",numeFisCss ))// +(new Date()).getTime()
+    }
+    rez=sass.compile(caleScss, {"sourceMap":true});
+    fs.writeFileSync(caleCss,rez.css)
+    
+}
+
+
+//la pornirea serverului
+vFisiere=fs.readdirSync(obGlobal.folderScss);
+for( let numeFis of vFisiere ){
+    if (path.extname(numeFis)==".scss"){
+        compileazaScss(numeFis);
+    }
+}
+
+
+fs.watch(obGlobal.folderScss, function(eveniment, numeFis){
+    if (eveniment=="change" || eveniment=="rename"){
+        let caleCompleta=path.join(obGlobal.folderScss, numeFis);
+        if (fs.existsSync(caleCompleta)){
+            compileazaScss(caleCompleta);
+        }
+    }
+})
+
+function afisareEroare(res, identificator, titlu, text, imagine){
+    //TO DO cautam eroarea dupa identificator
+    //daca sunt setate titlu, text, imagine, le folosim, 
+    //altfel folosim cele din fisierul json pentru eroarea gasita
+    //daca nu o gasim, afisam eroarea default
+
+}
 
 
 app.listen(8080);
